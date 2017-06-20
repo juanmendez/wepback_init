1ef7dc1f5312af22ef2c9e5e983bf1a3069fce1e
Juan Mendez <contact@juanmendez.info>
2017-06-19 23:53:41 -0500

    refreshing my head around typescript tsd...
    about a year ago I used TS. I forgot certain configurations. I am
    reviewing things now.

--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@
  * Created by juan on 7/6/2016.
  */
 import {login} from './login';
+import * as $ from "jquery";
 
-document.write( "working with typescript can be fun!");
-console.log( "app loaded" );
+$(()=>{
+    document.write( "working with typescript can be fun!");
+    console.log( "app loaded" );
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ module.exports = {
         loaders:[
             {
                 test: /\.tsx?$/,
-                exclude: /node_modules/,
+                exclude: ['node_modules','typings'],
                 loader: "ts-loader"
             },
             {
                 test: /\.js/,
-                exclude: 'node_modules',
+                exclude: ['node_modules','typings'],
                 loader: 'jshint-loader',
                 enforce:"pre"
             }
